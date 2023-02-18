b60fb9f655d16e9f6c8440e011828272580386cd
Ezequiel Barraza <barrazaezequielg@gmail.com>
2023-02-18 13:27:34 -0300

    Desafio entregable 3 - ExpressJS

---
+++ Desafio 3  ExpressJS/src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import ProductManager from "./ProductManager.js";
+
+const app = express();
+app.use(express.json());
+
+const productManager = new ProductManager("./Fiambres.json");
+
+app.get("/products", async (req, res) => {
+  const { limit } = req.query;
+  const product = await productManager.getProducts();
+  if (!limit) {
+    res.send(product);
+  } else {
+    res.send(product.slice(0, limit));
+  }
+});
+
+app.get("/products/:pid", async (req, res) => {
+  const pid = parseInt(req.params.pid);
+  const product = await productManager.getProducts();
+  const prodEncontrado = product.find((el) => {
+    return el.idProduct === pid;
+  });
+
+  if (prodEncontrado) {
+    res.send(prodEncontrado);
+  } else {
+    res.send(["Producto no existente"]);
+  }
+});
+
+app.listen(8082, () => {
+  console.log("listening in 8082");
+});
